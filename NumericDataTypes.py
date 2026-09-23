8a9370bc167b0aa1223e1e5e2424899af91a350f
Ronohh <cimanu88@gmail.com>
2026-09-23 19:58:46 +0300

    working with Numeric Data Types

---
+++ NumericDataTypes.py
@@ -0,0 +1,26 @@
+# Use the Python shell
+
+# Use the int data type
+
+# Use the float data type
+
+# Use the complex data type
+
+# Use the bool data type
+
+
+print("Python has three numeric types: int, float, and complex")
+
+# Creating a variable
+
+myValue=1
+print(myValue)
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Introducing the float data type
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
