402cb9f63b11dbabe67d7aa0cf98a325c8f57c85
Ronohh <cimanu88@gmail.com>
2026-09-23 20:32:50 +0300

    The bool data type

--- NumericDataTypes.py
+++ NumericDataTypes.py
@@ -30,3 +30,14 @@ myValue=5j
 print(myValue)
 print(type(myValue))
 print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Introducing the bool data type
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
